794509b6f8b2a64ac6bbdcbd4dc4d67f130e7a9a
approached <alexejkloos@gmail.com>
2016-10-10 21:28:55 +0200

    + powerimage config

--- config/filesystems.php
+++ config/filesystems.php
@@ -62,6 +62,11 @@ return [
             'bucket' => 'your-bucket',
         ],
 
+        'powerimage' => [
+            'driver' => 'local',
+            'root'   => storage_path('powerimage'),
+        ],
+
     ],
 
 ];
