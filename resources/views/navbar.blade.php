4ace9122b86bb8de732ad109846c0f128e594935
approached <alexejkloos@gmail.com>
2016-10-14 21:59:25 +0200

    - admin dropdown menu

--- resources/views/navbar.blade.php
+++ resources/views/navbar.blade.php
@@ -28,42 +28,6 @@
                         <a href="{{ route('user.login') }}">Login</a>
                     </li>
                 @endif
-
-                @if(Auth::user() && Auth::user()->hasRole('admin'))
-                    <li class="dropdown">
-                        <a href="#" class="dropdown-toggle" data-toggle="dropdown" role="button" aria-haspopup="true"
-                           aria-expanded="false">Admin <span class="caret"></span></a>
-                        <ul class="dropdown-menu">
-                            @if(view()->exists('page::cmf.menu'))
-                                @include('page::cmf.menu')
-                            @endif
-                            @if(view()->exists('gallery::cmf.menu'))
-                                <li role="separator" class="divider"></li>
-                                @include('gallery::cmf.menu')
-                            @endif
-                            @if(view()->exists('user::cmf.menu'))
-                                <li role="separator" class="divider"></li>
-                                @include('user::cmf.menu')
-                            @endif
-                            @if(view()->exists('video::cmf.menu'))
-                                <li role="separator" class="divider"></li>
-                                @include('video::cmf.menu')
-                            @endif
-                            @if(view()->exists('block::cmf.menu'))
-                                <li role="separator" class="divider"></li>
-                                @include('block::cmf.menu')
-                            @endif
-                            @if(view()->exists('menu::cmf.menu'))
-                                <li role="separator" class="divider"></li>
-                                @include('menu::cmf.menu')
-                            @endif
-                            @if(view()->exists('forum::cmf.menu'))
-                                <li role="separator" class="divider"></li>
-                                @include('forum::cmf.menu')
-                            @endif
-                        </ul>
-                    </li>
-                @endif
             </ul>
         </div>
     </div>
